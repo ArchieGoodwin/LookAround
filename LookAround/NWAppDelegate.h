fce047adbcee6c7e6e5bd2fb40607078aeab9775
Sergey Dikarev <nerowolfe@me.com>
2013-02-08 10:28:05 +0400

    Initial Commit

---
+++ LookAround/NWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NWAppDelegate.h
+//  LookAround
+//
+//  Created by Sergey Dikarev on 2/8/13.
+//  Copyright (c) 2013 Sergey Dikarev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
